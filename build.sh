b870b4c2749e8b4479ae91c967aef53ba23ec274
Nayeem-Akhta <akhtarnayeem2003@gmail.com>
2026-05-27 15:57:39 +0530

    Fix wsgi, remove staticfiles from git, let Render generate them

--- build.sh
+++ build.sh
@@ -2,13 +2,13 @@
 set -o errexit
 
 pip install -r requirements.txt
-python manage.py collectstatic --no-input
+python manage.py collectstatic --no-input --clear
 python manage.py migrate
 python manage.py shell -c "
 from core.models import User, Organization
 if not User.objects.filter(username='admin').exists():
     org, _ = Organization.objects.get_or_create(name='Test Company', slug='test-company')
-    user = User.objects.create_superuser('admin3', 'admin@breatheesg.com', 'Admin@123')
+    user = User.objects.create_superuser('admin4', 'admin@breatheesg.com', 'Admin@123')
     user.organization = org
     user.save()
     print('Superuser and organization created')
